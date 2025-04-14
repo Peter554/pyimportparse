fcf8993380c41f39f313f07e8df802b84503c516
Peter Byfield <peter.byfield@kraken.tech>
2025-04-14 06:11:21 +0200

    Refactor tests

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,12 @@ struct GrammarParser;
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct Import {
     pub imported_object: String,
-    pub line_number: usize,
+    pub line_number: u32,
     pub typechecking_only: bool,
 }
 
 impl Import {
-    pub fn new(imported_object: &str, line_number: usize, typechecking_only: bool) -> Self {
+    pub fn new(imported_object: &str, line_number: u32, typechecking_only: bool) -> Self {
         Self {
             imported_object: imported_object.to_owned(),
             line_number,
@@ -84,7 +84,7 @@ fn parse_simple_import_statement(pair: Pair<Rule>, context: &mut ParseContext) -
                 let imported_object = inner_pair.as_str().to_owned();
                 Some(Import {
                     imported_object,
-                    line_number,
+                    line_number: line_number as u32,
                     typechecking_only: context.typechecking_only,
                 })
             }
@@ -111,7 +111,7 @@ fn parse_from_import_statement(pair: Pair<Rule>, context: &mut ParseContext) -> 
                 let imported_object = format!("{}.{}", imported_base, inner_pair.as_str());
                 Some(Import {
                     imported_object,
-                    line_number,
+                    line_number: line_number as u32,
                     typechecking_only: context.typechecking_only,
                 })
             }
@@ -134,244 +134,343 @@ fn parse_wildcard_from_import_statement(
     let imported = format!("{}.*", imported_l);
     vec![Import {
         imported_object: imported.to_string(),
-        line_number,
+        line_number: line_number as u32,
         typechecking_only: context.typechecking_only,
     }]
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Import, parse_imports};
+    use super::parse_imports;
     use parameterized::parameterized;
 
-    struct ParseTestCase<'a> {
-        code: &'a str,
-        expected_imports: &'a [Import],
+    #[test]
+    fn test_parse_empty_string() {
+        let imports = parse_imports("").unwrap();
+        assert!(imports.is_empty());
+    }
+
+    fn parse_and_check(case: (&str, &[&str])) {
+        let (code, expected_imports) = case;
+        let imports = parse_imports(code).unwrap();
+        assert_eq!(
+            expected_imports,
+            imports
+                .into_iter()
+                .map(|i| i.imported_object)
+                .collect::<Vec<_>>()
+        );
+    }
+
+    fn parse_and_check_with_typechecking_only(case: (&str, &[(&str, bool)])) {
+        let (code, expected_imports) = case;
+        let imports = parse_imports(code).unwrap();
+        assert_eq!(
+            expected_imports
+                .iter()
+                .map(|i| (i.0.to_owned(), i.1))
+                .collect::<Vec<_>>(),
+            imports
+                .into_iter()
+                .map(|i| (i.imported_object, i.typechecking_only))
+                .collect::<Vec<_>>()
+        );
     }
 
     #[parameterized(case = {
-        ParseTestCase {
-            code: "",
-            expected_imports: &[],
-        },
-        ParseTestCase {
-            code: "import foo",
-            expected_imports: &[Import::new("foo", 1, false)],
-        },
-        ParseTestCase {
-            code: "import foo_bar",
-            expected_imports: &[Import::new("foo_bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "import foo.bar",
-            expected_imports: &[Import::new("foo.bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "import foo as foofoo",
-            expected_imports: &[Import::new("foo", 1, false)],
-        },
-        ParseTestCase {
-            code: "import foo, bar",
-            expected_imports: &[
-                Import::new("foo", 1, false),
-                Import::new("bar", 1, false)
-            ],
-        },
-        ParseTestCase {
-            code: "import foo; import bar",
-            expected_imports: &[
-                Import::new("foo", 1, false),
-                Import::new("bar", 1, false)
-            ],
-        },
-        ParseTestCase {
-            code: "import foo; import bar;",
-            expected_imports: &[
-                Import::new("foo", 1, false),
-                Import::new("bar", 1, false)
-            ],
-        },
-        ParseTestCase {
-            code: "
-import a
-import b.c",
-            expected_imports: &[
-                Import::new("a", 2, false),
-                Import::new("b.c", 3, false)
-            ],
-        },
-        ParseTestCase {
-            code: "from foo import bar",
-            expected_imports: &[Import::new("foo.bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "from foo import bar as barbar",
-            expected_imports: &[Import::new("foo.bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "from .foo import bar",
-            expected_imports: &[Import::new(".foo.bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "from ..foo import bar",
-            expected_imports: &[Import::new("..foo.bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "from . import foo",
-            expected_imports: &[Import::new(".foo", 1, false)],
-        },
-        ParseTestCase {
-            code: "from .. import foo",
-            expected_imports: &[Import::new("..foo", 1, false)],
-        },
-        ParseTestCase {
-            code: "import foo; from bar import baz",
-            expected_imports: &[
-                Import::new("foo", 1, false),
-                Import::new("bar.baz", 1, false)
-            ],
-        },
-        ParseTestCase {
-            code: "from foo import *",
-            expected_imports: &[Import::new("foo.*", 1, false)],
-        },
-        ParseTestCase {
-            code: "from . import *",
-            expected_imports: &[Import::new(".*", 1, false)],
-        },
-        ParseTestCase {
-            code: "from .. import *",
-            expected_imports: &[Import::new("..*", 1, false)],
-        },
-        ParseTestCase {
-            code: "from foo import bar, baz",
-            expected_imports: &[
-                Import::new("foo.bar", 1, false),
-                Import::new("foo.baz", 1, false)
-            ],
-        },
-        ParseTestCase {
-            code: "from foo import (bar)",
-            expected_imports: &[Import::new("foo.bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "from foo import (bar,)",
-            expected_imports: &[Import::new("foo.bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "from foo import (bar, baz)",
-            expected_imports: &[
-                Import::new("foo.bar", 1, false),
-                Import::new("foo.baz", 1, false)
-            ],
-        },
-        ParseTestCase {
-            code: "from foo import (bar, baz,)",
-            expected_imports: &[
-                Import::new("foo.bar", 1, false),
-                Import::new("foo.baz", 1, false)
-            ],
-        },
-        ParseTestCase {
-            code: "
+        ("import foo", &["foo"]),
+        ("import foo_FOO_123", &["foo_FOO_123"]),
+        ("import foo.bar", &["foo.bar"]),
+        ("import foo.bar.baz", &["foo.bar.baz"]),
+        ("import foo, bar, bax", &["foo", "bar", "bax"]),
+        ("import foo as FOO", &["foo"]),
+        ("import foo as FOO, bar as BAR", &["foo", "bar"]),
+        ("import  foo  as  FOO ,  bar  as  BAR", &["foo", "bar"]),
+        ("import foo # Comment", &["foo"]),
+    })]
+    fn test_parse_simple_import_statement(case: (&str, &[&str])) {
+        parse_and_check(case);
+    }
+
+    #[parameterized(case = {
+        ("from foo import bar", &["foo.bar"]),
+        ("from foo import bar_BAR_123", &["foo.bar_BAR_123"]),
+        ("from .foo import bar", &[".foo.bar"]),
+        ("from ..foo import bar", &["..foo.bar"]),
+        ("from . import foo", &[".foo"]),
+        ("from .. import foo", &["..foo"]),
+        ("from foo.bar import baz", &["foo.bar.baz"]),
+        ("from .foo.bar import baz", &[".foo.bar.baz"]),
+        ("from ..foo.bar import baz", &["..foo.bar.baz"]),
+        ("from foo import bar, baz, bax", &["foo.bar", "foo.baz", "foo.bax"]),
+        ("from foo import bar as BAR", &["foo.bar"]),
+        ("from foo import bar as BAR, baz as BAZ", &["foo.bar", "foo.baz"]),
+        ("from  foo  import  bar  as  BAR ,  baz  as  BAZ", &["foo.bar", "foo.baz"]),
+        ("from foo import bar # Comment", &["foo.bar"]),
+    })]
+    fn test_parse_from_import_statement(case: (&str, &[&str])) {
+        parse_and_check(case);
+    }
+
+    #[parameterized(case = {
+        ("from foo import (bar)", &["foo.bar"]),
+        ("from foo import (bar,)", &["foo.bar"]),
+        ("from foo import (bar, baz)", &["foo.bar", "foo.baz"]),
+        ("from foo import (bar, baz,)", &["foo.bar", "foo.baz"]),
+        ("from foo import (bar as BAR, baz as BAZ,)", &["foo.bar", "foo.baz"]),
+        ("from  foo  import  ( bar  as  BAR , baz  as  BAZ , )", &["foo.bar", "foo.baz"]),
+        ("from foo import (bar, baz,) # Comment", &["foo.bar", "foo.baz"]),
+
+        (r#"
 from foo import (
-    bar, baz
-)",
-            expected_imports: &[Import::new("foo.bar", 2, false), Import::new("foo.baz", 2, false)],
-        },
-        ParseTestCase {
-            code: r"from \
-    foo \
-    import \
-    bar",
-            expected_imports: &[Import::new("foo.bar", 1, false)],
-        },
-        ParseTestCase {
-            code: "
-import typing
-if typing.TYPE_CHECKING:
-    import foo
-import bar",
-            expected_imports: &[
-    Import::new("typing", 2, false),
-    Import::new("foo", 4, true),
-    Import::new("bar", 5, false),
-],
-        },
-        ParseTestCase {
-            code: "
-import typing
-if typing.TYPE_CHECKING:
-    import foo",
-            expected_imports: &[
-    Import::new("typing", 2, false),
-    Import::new("foo", 4, true),
-],
-        },
-        ParseTestCase {
-            code: "
-import typing
-if typing.TYPE_CHECKING:
-    print(\"hello\")
-    import foo",
-            expected_imports: &[
-    Import::new("typing", 2, false),
-    Import::new("foo", 5, true),
-],
-        },
-        ParseTestCase {
-            code: "
-import typing
+    bar,
+    baz
+)
+        "#, &["foo.bar", "foo.baz"]),
 
+        (r#"
+from foo import (
+    bar,
+    baz,
+)
+        "#, &["foo.bar", "foo.baz"]),
+
+        (r#"
+from foo import (
+    a, b,
+    c, d,
+)
+        "#, &["foo.a", "foo.b", "foo.c", "foo.d"]),
+
+        // As name
+        (r#"
+from foo import (
+    bar as BAR,
+    baz as BAZ,
+)
+        "#, &["foo.bar", "foo.baz"]),
+
+        // Whitespace
+        (r#"
+from  foo  import  (
+
+    bar  as  BAR ,
+
+       baz  as  BAZ ,
+
+)
+        "#, &["foo.bar", "foo.baz"]),
+
+        // Comments
+        (r#"
+from foo import ( # C
+    # C
+    bar as BAR, # C
+    # C
+    baz as BAZ, # C
+    # C
+) # C
+        "#, &["foo.bar", "foo.baz"]),
+    })]
+    fn test_parse_multiline_from_import_statement(case: (&str, &[&str])) {
+        parse_and_check(case);
+    }
+
+    #[parameterized(case = {
+        ("from foo import *", &["foo.*"]),
+        ("from .foo import *", &[".foo.*"]),
+        ("from ..foo import *", &["..foo.*"]),
+        ("from . import *", &[".*"]),
+        ("from .. import *", &["..*"]),
+        ("from  foo  import  *", &["foo.*"]),
+        ("from foo import * # Comment", &["foo.*"]),
+    })]
+    fn test_parse_wildcard_from_import_statement(case: (&str, &[&str])) {
+        parse_and_check(case);
+    }
+
+    #[parameterized(case = {
+        ("import a; import b", &["a", "b"]),
+        ("import a; import b;", &["a", "b"]),
+        ("import  a ;  import  b ;", &["a", "b"]),
+        ("import a; import b # Comment", &["a", "b"]),
+        ("import a; from b import c; from d import (e); from f import *", &["a", "b.c", "d.e", "f.*"]),
+    })]
+    fn test_parse_import_statement_list(case: (&str, &[&str])) {
+        parse_and_check(case);
+    }
+
+    #[parameterized(case = {
+        (r#"
+import a, b, \
+       c, d
+        "#, &["a", "b", "c", "d"]),
+
+        (r#"
+from foo import a, b, \
+                c, d
+        "#, &["foo.a", "foo.b", "foo.c", "foo.d"]),
+
+        (r#"
+from foo \
+    import *
+        "#, &["foo.*"]),
+    })]
+    fn test_backslash_continuation(case: (&str, &[&str])) {
+        parse_and_check(case);
+    }
+
+    #[parameterized(case = {
+        (r#"
+import a
+def foo():
+    import b 
+import c
+        "#, &["a", "b", "c"]),
+
+        (r#"
+import a
+class Foo:
+    import b
+import c
+        "#, &["a", "b", "c"]),
+    })]
+    fn test_parse_nested_imports(case: (&str, &[&str])) {
+        parse_and_check(case);
+    }
+
+    #[parameterized(case = {
+        (r#"
+import foo
+if typing.TYPE_CHECKING: import bar
+import baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+
+        (r#"
+import foo
+if TYPE_CHECKING: import bar
+import baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+
+        (r#"
+import foo
+if  TYPE_CHECKING :  import bar
+import baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+
+        (r#"
+import foo
+if TYPE_CHECKING: import bar as BAR
+import baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+
+        (r#"
+import foo # C
+if TYPE_CHECKING: import bar # C
+import baz # C
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+    })]
+    fn test_singleline_if_typechecking(case: (&str, &[(&str, bool)])) {
+        parse_and_check_with_typechecking_only(case);
+    }
+
+    #[parameterized(case = {
+        (r#"
+import foo
 if typing.TYPE_CHECKING:
+    import bar
+import baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
 
-    import foo
+        (r#"
+import foo
+if TYPE_CHECKING:
+    import bar
+import baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
 
-import bar",
-            expected_imports: &[
-    Import::new("typing", 2, false),
-    Import::new("foo", 6, true),
-    Import::new("bar", 8, false),
-],
-        },
-        ParseTestCase {
-            code: "import foo  # hello",
-            expected_imports: &[Import::new("foo", 1, false)],
-        },
-        ParseTestCase {
-            code: r#"
-"""
+        (r#"
+import  foo
+
+if  TYPE_CHECKING :
+
+    import  bar
+
+import  baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+
+        (r#"
+import foo
+if TYPE_CHECKING:
+    import bar as BAR
+import baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+
+        (r#"
+import foo # C
+if TYPE_CHECKING: # C
+    # C
+    import bar # C
+    # C
+import baz # C
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+
+        (r#"
+import foo
+if TYPE_CHECKING:
+    """
+    Comment
+    """
+    import bar
+import baz
+"#, &[("foo", false), ("bar", true), ("baz", false)]),
+    })]
+    fn test_multiline_if_typechecking(case: (&str, &[(&str, bool)])) {
+        parse_and_check_with_typechecking_only(case);
+    }
+
+    #[parameterized(case = {
+        (r#"
 import foo
 """
-import bar"#,
-            expected_imports: &[Import::new("bar", 5, false)],
-        },
-        ParseTestCase {
-            code: r#"
-if TYPE_CHECKING: # Only for typechecking
-    import foo"#,
-            expected_imports: &[Import::new("foo", 3, true)],
-        },
-        ParseTestCase {
-            code: r#"
-if TYPE_CHECKING: import foo  # comment
-import bar"#,
-            expected_imports: &[
-                Import::new("foo", 2, true),
-                Import::new("bar", 3, false)
-            ],
-        },
-        ParseTestCase {
-            code: r#"
-def f():
-    import foo"#,
-            expected_imports: &[
-                Import::new("foo", 3, false),
-            ],
-        },
+import bar
+"""
+import baz
+"#, &["foo", "baz"]),
+
+        (r#"
+import foo
+'''
+import bar
+'''
+import baz
+"#, &["foo", "baz"]),
     })]
-    fn test_parse(case: ParseTestCase) {
-        let result = parse_imports(case.code);
-        pretty_assertions::assert_eq!(Ok(case.expected_imports.to_vec()), result);
+    fn test_multiline_strings(case: (&str, &[&str])) {
+        parse_and_check(case);
+    }
+
+    #[test]
+    fn test_parse_line_numbers() {
+        let imports = parse_imports(
+            "
+import a
+from b import c
+from d import (e)
+from f import *",
+        )
+        .unwrap();
+        assert_eq!(
+            vec![
+                ("a".to_owned(), 2_u32),
+                ("b.c".to_owned(), 3_u32),
+                ("d.e".to_owned(), 4_u32),
+                ("f.*".to_owned(), 5_u32),
+            ],
+            imports
+                .into_iter()
+                .map(|i| (i.imported_object, i.line_number))
+                .collect::<Vec<_>>()
+        );
     }
 }
